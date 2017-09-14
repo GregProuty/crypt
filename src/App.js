d3c909c9634afd5c6b1038a842af4c3a400495a1
GregoryProuty <g.prout009@gmail.com>
2017-09-14 13:15:21 -0700

    lifted state up to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import SearchInput /*, {createFilter}*/ from 'react-search-input'
 import './App.css';
 // fetch('https://api.cryptonator.com/api/full/btc-usd').then((res) => res.json()).then((json) => console.log(json));
 
@@ -7,18 +6,63 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      display: 'master',
+      masterData: [],
+      individualData: [],
+      searchValue: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event) {
+    fetch('https://api.cryptonator.com/api/full/' + this.state.searchValue + '-usd')
+      .then((res) => {
+        res.json()
+        alert('then')
+      })
+      .then((json) => {
+        alert('????')
+        console.log(json)
+        this.setState({
+          individualData: json,
+          display: 'individual'
+        })
+    });
+  }
+  handleChange(event) {
+    this.setState({searchValue: event.target.value});
+  }
+  getData() {
+    fetch('https://api.coinmarketcap.com/v1/ticker/')
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({masterData: json})
+    });
+  }
+  componentWillMount() {
+    this.getData()
   }
   render() {
+    var table = <Table masterData={this.state.masterData} display={this.state.display} individualData={this.state.individualData} />
+    if(this.state.display === 'individual') {
+      table = () => {
+        return <T2 />
+      }
+    }
     return (
       <div className="App">
         <div className="navbar navbar-default">
           <h2>Crypt</h2>
-          <Search />
+          <form onSubmit={this.handleSubmit} className='search'>
+            <label>
+              Search:<br></br> 
+              <input type="text" value={this.state.searchValue} onSubmit={this.handleSubmit} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" onSubmit={this.handleSubmit} />
+          </form>
         </div>
         <div className="table">
-          <Table />
+        {table}
         </div>
       </div>
     );
@@ -26,20 +70,9 @@ class App extends Component {
 }
 
 class Table extends Component {
-  constructor() {
-    super();
-    this.state = {
-      masterData: []
-    }
-  }
-  getData() {
-    fetch('https://api.coinmarketcap.com/v1/ticker/').then((res) => res.json()).then((json) => {
-      this.setState({masterData: json})
-    });
-  }
   sortPrice(){ 
     this.setState({
-          masterData: this.state.masterData.sort(function(a, b) {
+          masterData: this.props.masterData.sort(function(a, b) {
           return b.price_usd - a.price_usd;
         })
     })
@@ -47,26 +80,22 @@ class Table extends Component {
   }
   sortMarketCap(){
     this.setState({
-      masterData: this.state.masterData.sort(function(a,b) {
+      masterData: this.props.masterData.sort(function(a,b) {
         return b.market_cap_usd - a.market_cap_usd;
       })
     })
   }
   sortName(){
     this.setState({
-      masterData: this.state.masterData.sort(function(a, b) {
+      masterData: this.props.masterData.sort(function(a, b) {
         if(a.name < b.name) return -1;
         if(a.name > b.name) return 1;
         return 0;
       })
     })
   }
-  componentWillMount() {
-    this.getData()
-  }
   render() {
     return (
-      
       <table className='table-hover'>
         <thead className='buttonBar'>
           <tr>
@@ -75,19 +104,25 @@ class Table extends Component {
             <td><button className='btn btn-default' type="button" onClick={() => { this.sortMarketCap()}}>Market Cap</button></td>
           </tr>
         </thead>
-          {
-            this.state.masterData.map((listValue, i) => {
-              return <tbody key={i} className='parent'>
+          { 
+            this.props.masterData.map((listValue, i) => {
+              return  <tbody key={i} className='parent'>
                         <tr>
                           <td className='child'>{listValue.name + ' (' + listValue.symbol + ')' }</td>
                           <td className='child'>{listValue.price_usd}</td>
                           <td className='child'>{listValue.market_cap_usd}</td>
                         </tr>
                       </tbody>
-            })
-          }
+              })
+            }
       </table>
     );
+  }
+}
+
+class T2 extends Component {
+  render() {
+    return <h3>T2</h3>
   }
 }
 
@@ -102,34 +137,34 @@ class Table extends Component {
 //   }
 // }
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+// class Search extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
 
-  handleSubmit(event) {
-    fetch('https://api.cryptonator.com/api/full/' + this.state.value + '-usd').then((res) => res.json()).then((json) => console.log(json));
-    event.preventDefault();
-  }
+//   handleSubmit(event) {
+//     fetch('https://api.cryptonator.com/api/full/' + this.state.value + '-usd').then((res) => res.json()).then((json) => console.log(json));
+//     event.preventDefault();
+//   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className='search'>
-        <label>
-          Search:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} className='search'>
+//         <label>
+//           Search:
+//           <input type="text" value={this.state.value} onChange={this.handleChange} />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
 export default App;
